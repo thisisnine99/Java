02738d7a3f6c775436a4ee22b37ca07a43eae768
thisisnine99 <dkdlel2099>
2023-09-08 17:48:40 +0900

    인터페이스 예제

---
+++ day8/Interface/InterfaceExam1/InterfaceExam1.java
@@ -0,0 +1,35 @@
+package day8.Interface;
+
+public class Interface {
+    public static void main(String[] args) {
+        ZooKeeper z1 = new ZooKeeper();
+        Lion L1 = new Lion();
+        z1.feed(L1);
+
+    }
+}
+
+class ZooKeeper {
+    void feed(Predator name) {
+        System.out.printf("feed %s\n", name.getFood());
+    }
+}
+interface Predator {
+    String getFood();
+}
+class Animal {
+    String name;
+    void setName(String name) {
+        this.name = name;
+    }
+}
+class Lion extends Animal implements Predator {
+    public String getFood() {
+        return "pig";
+    }
+}
+class Tigger extends Animal implements Predator {
+    public String getFood() {
+        return "cow";
+    }
+}
---
+++ day8/Interface/InterfaceExam2/InterfaceExam2.java
@@ -0,0 +1,2 @@
+package day8.Interface;public class InterfaceExam2 {
+}
